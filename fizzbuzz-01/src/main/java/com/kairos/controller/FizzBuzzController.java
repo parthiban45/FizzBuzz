cf4ee5d0e87d335bf3403895e04dbbe470b5a2f0
parthiban45 <parthi45.psg@gmail.com>
2018-02-21 10:58:16 +0000

    update FizzBuzzController

--- fizzbuzz-01/src/main/java/com/kairos/controller/FizzBuzzController.java
+++ fizzbuzz-01/src/main/java/com/kairos/controller/FizzBuzzController.java
@@ -8,16 +8,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.kairos.service.FizzBuzzService;
 
+/* FizzBuzz Rest Controller, Calls underlying business service 
+to get the response */
 @RestController
 public class FizzBuzzController {
 
 	@Autowired
 	private FizzBuzzService fizzBuzzService;
 
+	/* Default constructor */
 	public FizzBuzzController() {
-
 	}
 
+	/* Method to return a word based on the following constraints
+	- Input number divisible by 15, return "FizzBuzz"
+	- Input number divisible by 3, return "Fizz"
+	- Input number divisible by 5, return "Buzz"
+	- Input number not divisible by either of them above, return the input integer
+	- By calling the underlying FizzBuzzService
+	*/
 	@RequestMapping(value = "/word/{number}", method = RequestMethod.GET, produces = "application/json")
 	public String getWord(@PathVariable(value="number") Integer i) {
 		return fizzBuzzService.FizzBuzz(i);
