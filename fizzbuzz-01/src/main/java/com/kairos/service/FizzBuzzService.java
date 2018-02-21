561a7f41203900729dd6cdaf780a4dc5d2e7f7cc
parthiban45 <parthi45.psg@gmail.com>
2018-02-21 11:02:54 +0000

    update FizzBuzzService

--- fizzbuzz-01/src/main/java/com/kairos/service/FizzBuzzService.java
+++ fizzbuzz-01/src/main/java/com/kairos/service/FizzBuzzService.java
@@ -5,8 +5,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class FizzBuzzService {
 
+	/* Method to return a word based on the following constraints
+	- Input number divisible by 15, return "FizzBuzz"
+	- Input number divisible by 3, return "Fizz"
+	- Input number divisible by 5, return "Buzz"
+	- Input number not divisible by either of them above, return the input integer
+	- i/p - Integer
+	- o/p - String
+	*/
 	public String FizzBuzz(Integer i) {
-
 		String result = "";
 
 		if (i % 15 == 0) {
@@ -18,7 +25,7 @@ public class FizzBuzzService {
 		} else {
 			result = String.valueOf(i);
 		}
-
+		
 		return result;
 	}
 
